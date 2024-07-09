4ff3b30a3215b5d9b29923fbf48a71fcbbdafe4b
kennek4 <xktabz@gmail.com>
2024-07-09 14:39:35 -0600

    Moved API functions Moved the previously commented test api functions from WeatherRepository.kt to actual functions in RemoteWeatherDataSource.kt

--- app/src/main/java/com/kennek/skyspy/data/repositories/WeatherRepository.kt
+++ app/src/main/java/com/kennek/skyspy/data/repositories/WeatherRepository.kt
@@ -11,36 +11,4 @@ class WeatherRepository @Inject constructor(
     private val remoteWeatherDataSource: RemoteWeatherDataSource,
 ){
 
-//    suspend fun getCurrentWeather(
-//        lon: Float,
-//        lat: Float,
-//        units: String,
-//        lang: String,
-//        appId: String
-//    ): Resource<CurrentWeather> {
-//        val response = try {
-//            api.getCurrentWeather(lon, lat, units, lang, appId)
-//        } catch (error: Exception) {
-//            return Resource.Error(message = "An error has occurred. $error")
-//        }
-//
-//        return Resource.Success(response)
-//    }
-
-//    suspend fun getForecast(
-//        lon: Float,
-//        lat: Float,
-//        units: String,
-//        lang: String,
-//        appId: String
-//    ): Resource<Forecast> {
-//
-//        val response = try {
-//            api.getForecast(lon, lat, units, lang, appId)
-//        } catch (error: Exception) {
-//            return Resource.Error(message = "An error has occurred. $error")
-//        }
-//
-//        return Resource.Success(response)
-//    }
 }
--- app/src/main/java/com/kennek/skyspy/data/sources/RemoteWeatherDataSource.kt
+++ app/src/main/java/com/kennek/skyspy/data/sources/RemoteWeatherDataSource.kt
@@ -1,10 +1,45 @@
 package com.kennek.skyspy.data.sources
 
 import com.kennek.skyspy.data.remote.WeatherApi
+import com.kennek.skyspy.data.responses.CurrentWeather
+import com.kennek.skyspy.data.responses.Forecast
+import com.kennek.skyspy.util.Resource
 import javax.inject.Inject
 
 class RemoteWeatherDataSource @Inject constructor(
     private val api: WeatherApi
 ) {
 
+    suspend fun getCurrentWeather(
+        lon: Float,
+        lat: Float,
+        units: String,
+        lang: String,
+        appId: String
+    ): Resource<CurrentWeather> {
+        val response = try {
+            api.getCurrentWeather(lon, lat, units, lang, appId)
+        } catch (error: Exception) {
+            return Resource.Error(message = "An error has occurred. $error")
+        }
+
+        return Resource.Success(response)
+    }
+
+    suspend fun getForecast(
+        lon: Float,
+        lat: Float,
+        units: String,
+        lang: String,
+        appId: String
+    ): Resource<Forecast> {
+
+        val response = try {
+            api.getForecast(lon, lat, units, lang, appId)
+        } catch (error: Exception) {
+            return Resource.Error(message = "An error has occurred. $error")
+        }
+
+        return Resource.Success(response)
+    }
 }
