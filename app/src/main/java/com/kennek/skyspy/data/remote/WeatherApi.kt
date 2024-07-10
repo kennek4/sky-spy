ce549b0eb88c13e8ce8b5b319209529c5745ab03
kennek4 <xktabz@gmail.com>
2024-07-10 02:48:28 -0600

    Updated @GET annotation Removed the replace blocks and left only the path to the desired API

--- app/src/main/java/com/kennek/skyspy/data/remote/WeatherApi.kt
+++ app/src/main/java/com/kennek/skyspy/data/remote/WeatherApi.kt
@@ -2,13 +2,12 @@ package com.kennek.skyspy.data.remote
 
 import com.kennek.skyspy.data.responses.CurrentWeather
 import com.kennek.skyspy.data.responses.Forecast
-import com.kennek.skyspy.data.responses.Weather
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
 
-    @GET("data/2.5/weather?lat={lat}&lon={lon}&units={units}&lang={lang}&appid={appId}")
+    @GET("data/2.5/weather")
     suspend fun getCurrentWeather(
         @Query("lon") lon: Float,
         @Query("lat") lat: Float,
@@ -17,7 +16,7 @@ interface WeatherApi {
         @Query("appId") appId: String
     ): CurrentWeather
 
-    @GET("data/2.5/forecast?lat={lat}&lon={lon}&units={units}&lang={lang}&appid={appId}")
+    @GET("data/2.5/forecast")
     suspend fun getForecast(
         @Query("lon") lon: Float,
         @Query("lat") lat: Float,
