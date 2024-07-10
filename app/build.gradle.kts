080342885b00d58f82b5b10f08fe29397fdd1fc3
kennek4 <xktabz@gmail.com>
2024-07-09 22:02:13 -0600

    Added comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    // Dagger Hilt
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
