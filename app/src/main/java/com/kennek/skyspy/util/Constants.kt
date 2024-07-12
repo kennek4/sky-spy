1d95970e6d79fefbab4070dc2e7d748a297dc6cf
kennek4 <xktabz@gmail.com>
2024-07-12 12:32:22 -0600

    Added: API parameter strings Created const strings representing parameters for the API

--- app/src/main/java/com/kennek/skyspy/util/Constants.kt
+++ app/src/main/java/com/kennek/skyspy/util/Constants.kt
@@ -2,4 +2,8 @@ package com.kennek.skyspy.util
 
 object Constants {
     const val BASE_URL = "https://api.open-meteo.com/v1/forecast/"
+    const val CURRENT = "current=temperature_2m,apparent_temperature,precipitation"
+    const val HOURLY = "hourly=temperature_2m,precipitation"
+    const val DAILY = "daily=temperature_2m_max,temperature_2m_min,sunrise,sunset"
+    const val TIMEZONE = "timezone=auto"
 }
