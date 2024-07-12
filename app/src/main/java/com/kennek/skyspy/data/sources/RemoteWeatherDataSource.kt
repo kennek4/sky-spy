f461fc43b872bc7e7dd1defefe0a16091cfeb4bb
kennek4 <xktabz@gmail.com>
2024-07-11 20:10:27 -0600

    Added: API call function Added the getForecast() to RemoteWeatherDataSource.kt

--- app/src/main/java/com/kennek/skyspy/data/sources/RemoteWeatherDataSource.kt
+++ app/src/main/java/com/kennek/skyspy/data/sources/RemoteWeatherDataSource.kt
@@ -1,10 +1,37 @@
 package com.kennek.skyspy.data.sources
 
 import com.kennek.skyspy.data.remote.WeatherApi
+import com.kennek.skyspy.data.responses.WeatherForecast
+import com.kennek.skyspy.util.Resource
 import javax.inject.Inject
 
 class RemoteWeatherDataSource @Inject constructor(
     private val api: WeatherApi
 ){
-
+    suspend fun getForecast(
+        latitude: Float,
+        longitude: Float,
+        current: String,
+        hourly: String,
+        daily: String,
+        timezone: String
+    ): Resource<WeatherForecast> {
+        try {
+            val response = api.getForecast(
+                latitude,
+                longitude,
+                current,
+                hourly,
+                daily,
+                timezone
+            )
+            return Resource.Success(
+                data = response
+            )
+        } catch (e: Exception) {
+            return Resource.Error(
+                message = "An error has occurred.\n$e"
+            )
+        }
+    }
 }
