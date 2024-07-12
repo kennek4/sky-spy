1947c18f489bb844833266c827c6cac24780db46
kennek4 <xktabz@gmail.com>
2024-07-12 02:54:53 -0600

    Added: PrimaryKey Forgot to add an ID the Settings Entity

--- app/src/main/java/com/kennek/skyspy/data/room/entities/Settings.kt
+++ app/src/main/java/com/kennek/skyspy/data/room/entities/Settings.kt
@@ -1,6 +1,7 @@
 package com.kennek.skyspy.data.room.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class Settings(
@@ -8,4 +9,6 @@ data class Settings(
     val longitude: String,
     val currentLocation: String,
     val units: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
 )
