f9755b975cbdb6391f343598bb425c52e7a7edf3
kennek4 <xktabz@gmail.com>
2024-07-12 17:40:29 -0600

    Changed: Constant values Removed the constants name from each respective string

--- app/src/main/java/com/kennek/skyspy/util/Constants.kt
+++ app/src/main/java/com/kennek/skyspy/util/Constants.kt
@@ -2,8 +2,8 @@ package com.kennek.skyspy.util
 
 object Constants {
     const val BASE_URL = "https://api.open-meteo.com/v1/forecast/"
-    const val CURRENT = "current=temperature_2m,apparent_temperature,precipitation"
-    const val HOURLY = "hourly=temperature_2m,precipitation"
-    const val DAILY = "daily=temperature_2m_max,temperature_2m_min,sunrise,sunset"
-    const val TIMEZONE = "timezone=auto"
+    const val CURRENT = "temperature_2m,apparent_temperature,precipitation"
+    const val HOURLY = "temperature_2m,precipitation"
+    const val DAILY = "temperature_2m_max,temperature_2m_min,sunrise,sunset"
+    const val TIMEZONE = "auto"
 }
