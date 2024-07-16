4329321be5088d71da3c0ad26092853692028703
kennek4 <xktabz@gmail.com>
2024-07-16 00:27:24 -0600

    Updated: WeatherAPI GET argument From BASE_URL to BASE_API_URL to distinguish from the Weather API URL and the GeoCode API URL

--- app/src/main/java/com/kennek/skyspy/data/remote/WeatherApi.kt
+++ app/src/main/java/com/kennek/skyspy/data/remote/WeatherApi.kt
@@ -1,12 +1,12 @@
 package com.kennek.skyspy.data.remote
 
 import com.kennek.skyspy.data.remote.responses.WeatherForecast
-import com.kennek.skyspy.util.Constants.BASE_URL
+import com.kennek.skyspy.util.Constants.BASE_API_URL
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
-    @GET(BASE_URL)
+    @GET(BASE_API_URL)
     suspend fun getForecast(
         @Query("latitude") latitude: Double,
         @Query("longitude") longitude: Double,
