e6f671f95e1ba60ac1e27e843d3f1e8c961461e7
kennek4 <xktabz@gmail.com>
2024-07-16 20:30:51 -0600

    Removed: Unused event

--- app/src/main/java/com/kennek/skyspy/data/events/LocationEvent.kt
+++ app/src/main/java/com/kennek/skyspy/data/events/LocationEvent.kt
@@ -1,7 +1,6 @@
 package com.kennek.skyspy.data.events
 
 sealed interface LocationEvent {
-    data object SaveLocation: LocationEvent
     data object GetLocation: LocationEvent
     data class SetLocation(val newLocation: String): LocationEvent
 }
